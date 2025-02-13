d0b5f24056b87fc11a87c7517b2049c6baddfb07
ShaniStaretz <shakimi88@gmail.com>
2025-02-13 18:08:59 +0200

    complete basic framework for the project include db functions and servers connections

--- db_functions.js
+++ db_functions.js
@@ -1,12 +1,82 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+const pg = require("pg");
 
 // PostgreSQL Connection Pool
-const connection = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+const connection = new pg.Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+console.debug("db.pool:", connection);
+check_onnection();
+
+connection.on("error", function (err, client) {
+  console.error("Idle client error", err.message, err.stack);
 });
 
-module.exports = connection;
+async function check_onnection() {
+  try {
+    console.debug("try db connection");
+    var client = await connection.connect();
+    console.info("db connection successful");
+  } catch (error) {
+    console.error(
+      "[db|" +
+        arguments.cllerr.name +
+        "] Error connecting to db" +
+        error.message
+    );
+  } finally {
+    client.release();
+  }
+  connection.removeAllListeners();
+}
+
+async function executeFunction(function_name, args) {
+  let res = { result: null, error: null };
+  try {
+    const client = await connection.connect();
+    try {
+      res.result = await client.query("SELECT * FROM" + function_name, args);
+    } catch (err) {
+      res.error = err.message;
+      console.error( "[db_queries|" + arguments.cllerr.name + "] Error calling db function" +  err.message
+      );
+    } finally {
+      client.release();
+    }
+  } catch (err) {
+    res.error = err.message;
+    console.error(
+      "[db_queries|" + arguments.cllerr.name + "] Error connecting to db" + err.message
+    );
+  }
+  connection.removeAllListeners();
+  return res;
+}
+
+async function executeQuery(query_Str) {
+    let res = { result: null, error: null };
+    try {
+      const client = await connection.connect();
+      try {
+        res.result = await client.query(query_Str);
+      } catch (err) {
+        res.error = err.message;
+        console.error( "[db_queries|" + arguments.cllerr.name + "] Error calling db query" +  err.message
+        );
+      } finally {
+        client.release();
+      }
+    } catch (err) {
+      res.error = err.message;
+      console.error(
+        "[db_queries|" + arguments.cllerr.name + "] Error connecting to db" + err.message
+      );
+    }
+    connection.removeAllListeners();
+    return res;
+  }
+module.exports = {executeFunction,executeQuery,connection};
--- monitorWorker.js
+++ monitorWorker.js
@@ -1,6 +1,7 @@
-   // Automated Worker to Monitor Server Status
- const monitorServerStatus = (connection) => {
-    console.log(connection)
+const axios = require('axios');
+ // Automated Worker to Monitor Server Status
+ 
+   const monitorServerStatus = (connection) => {
     console.log("start worker")
     const intervalId = setInterval(async () => {
         try {
@@ -16,5 +17,37 @@
 
     return intervalId; 
 };
+
+const parseUrl=(url) =>{
+    const regex =/^(ftp|http|https|ssh):\/\/([^:/]+)(?::(\d+))?/i;
+    const match = url.match(regex);
+    
+    if (match) {
+        const protocol = match[1]; // The protocol (http, https, ftp, ws, wss)
+        const host = match[2]; // The host (domain or IP address)
+        const port = match[3] || (protocol === 'https' ? '443' : protocol === 'http' ? '80' : protocol === 'ftp' ? '21' : '22'); // Default port based on protocol
+        
+        return { protocol, host, port };
+    }
+    
+    return null; // Return null if no match is found
+}
+
+const checkHttpConnection= async(protocol, host, port)=> {
+    const url = `${protocol}://${host}:${port}`;
+    const start = Date.now(); // Start time
+    try {
+        const response = await axios.get(url);
+        const latency = Date.now() - start; // Calculate latency
+        if (response.status === 200) {
+            console.log(`Connection to ${url} successful. Response Latency: ${latency}ms`);
+        } else {
+            console.log(`Received unexpected status code: ${response.status}. Latency: ${latency}ms`);
+        }
+    } catch (error) {
+        const latency = Date.now() - start;
+        console.error(`Error connecting to ${url}: ${error.message}. Latency: ${latency}ms`);
+    }
+}
 
 module.exports = monitorServerStatus;
