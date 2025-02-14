2c6eec75c6c935582228e97514acb3f1474939a7
ShaniStaretz <shakimi88@gmail.com>
2025-02-14 16:06:44 +0200

    change use of shut down server and pool

--- db/db_functions.js
+++ db/db_functions.js
@@ -1,46 +1,46 @@
-const pg = require("pg");
-
-// PostgreSQL Connection Pool
-const connection = new pg.Pool({
+const { Pool } = require("pg");
+// PostgreSQL pool Pool
+const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  max: 10, // Max number of connections in the pool
 });
 
 check_onnection();
 
-connection.on("error", function (err, client) {
+pool.on("error", function (err, client) {
   console.error("Idle client error", err.message, err.stack);
 });
 
 // Listen for notifications on the 'server_status_change' channel
-connection.query('LISTEN server_status_change');
+pool.query("LISTEN server_status_change");
 
 // Event listener for notifications
-connection.on('notification', (msg) => {
-    console.log('Received notification:', msg.payload);
+pool.on("notification", (msg) => {
+  console.log("Received notification:", msg.payload);
 
-    // Check if the notification is about an unhealthy server
-    if (msg.payload.includes('Unhealthy')) {
-        // Parse the server ID and status from the notification payload
-        const serverId = msg.payload.split(' ')[1];  // Assuming the message is like 'Server <id> is now Unhealthy'
-        const subject = `Alert: Server ${serverId} is Unhealthy`;
-        const text = `The server with ID ${serverId} is now marked as Unhealthy.`;
-        const emailTo = 'recipient@example.com';
+  // Check if the notification is about an unhealthy server
+  if (msg.payload.includes("Unhealthy")) {
+    // Parse the server ID and status from the notification payload
+    const serverId = msg.payload.split(" ")[1]; // Assuming the message is like 'Server <id> is now Unhealthy'
+    const subject = `Alert: Server ${serverId} is Unhealthy`;
+    const text = `The server with ID ${serverId} is now marked as Unhealthy.`;
+    const emailTo = "recipient@example.com";
 
-        // Send the email
-        sendEmail(subject, text, emailTo);
-    }
+    // Send the email
+    sendEmail(subject, text, emailTo);
+  }
 });
-console.log('Listening for PostgreSQL notifications...');
+console.log("Listening for PostgreSQL notifications...");
 
 async function check_onnection() {
   try {
-    console.debug("try db connection");
-    var client = await connection.connect();
-    console.info("db connection successful");
+    console.debug("try db pool");
+    var client = await pool.connect();
+    console.info("db pool successful");
   } catch (error) {
     console.error(
       "[db|" +
@@ -51,13 +51,13 @@ async function check_onnection() {
   } finally {
     client.release();
   }
-  connection.removeAllListeners();
+  pool.removeAllListeners();
 }
 
 async function executeFunction(is_get, function_name, args) {
   let res = { result: null, error: null };
   try {
-    const client = await connection.connect();
+    const client = await pool.connect();
     try {
       if (is_get) {
         res.result = await client.query("SELECT * FROM " + function_name, args);
@@ -84,14 +84,14 @@ async function executeFunction(is_get, function_name, args) {
         err.message
     );
   }
-  connection.removeAllListeners();
+  pool.removeAllListeners();
   return res;
 }
 
 async function executeQuery(query_Str) {
   let res = { result: null, error: null };
   try {
-    const client = await connection.connect();
+    const client = await pool.connect();
     try {
       res.result = await client.query(query_Str);
     } catch (err) {
@@ -114,7 +114,7 @@ async function executeQuery(query_Str) {
         err.message
     );
   }
-  connection.removeAllListeners();
+  pool.removeAllListeners();
   return res;
 }
-module.exports = { executeFunction, executeQuery, connection };
+module.exports = { executeFunction, executeQuery, pool };
--- server.js
+++ server.js
@@ -1,41 +1,40 @@
-require('dotenv').config();
-const express = require('express');
-const {connection} = require('./db/db_functions'); // Import the database pool
-const monitorServerStatus = require('./workers/monitor_worker');
-const shutdown =require('./utils/shutdown')
+require("dotenv").config();
+const express = require("express");
+
+const monitorServerStatus = require("./workers/monitor_worker");
+const shutdown = require("./utils/shutdown");
+
+const { monitorHistoryRoutes } = require("./routes/historyRoutes");
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-
 // Middleware
 app.use(express.json());
+// Use the routes
+// app.use(monitorHistoryRoutes);
 
- // Server Data Structure
- const serverData = {
-    name: 'Web Severs Monitoring Syatem',
-    url: `http://localhost:${port}`
+// Server Data Structure
+const serverData = {
+  name: "Web Severs Monitoring Syatem",
+  url: `http://localhost:${port}`,
 };
 
+const intervalId = monitorServerStatus();
 
-
-const intervalId=monitorServerStatus();
-
-
-
- // Test Route
- app.get('/', (req, res) => {
-    res.json({ message: 'Express + PostgreSQL API is running!', server: serverData });
+// Test Route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Express + PostgreSQL API is running!",
+    server: serverData,
+  });
 });
 
-// Start Server only if not in test mode
-if (require.main === module) {
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
-}
-// Handle graceful shutdown
-process.on('SIGINT', () => shutdown(connection, intervalId)); //signal interrupt
-process.on('SIGTERM', () => shutdown(connection, intervalId)); //signal Terminate
+const server = app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
-module.exports = {app,connection};
+// Handle graceful shutdown
+process.on("SIGINT", () => shutdown(server, intervalId)); //signal interrupt
+process.on("SIGTERM", () => shutdown(server, intervalId)); //signal Terminate
+
+module.exports = { app };
