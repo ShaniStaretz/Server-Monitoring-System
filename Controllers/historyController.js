9d395e28d485a3d08216482123720ea71756ea35
ShaniStaretz <shakimi88@gmail.com>
2025-02-15 13:13:36 +0200

    clean controller

--- Controllers/historyController.js
+++ Controllers/historyController.js
@@ -2,20 +2,19 @@ const {
   getExistServerHistory,
   getIsServerHealthy,
 } = require("../services/historyService");
+
 // Route handler function
 const handleGetExistServerHistory = async (req, res) => {
   const serverId = parseInt(req.params.serverId); // Get the serverId from the URL
 
   try {
-    // Call the getExistServerHistory function with the data
     const history = await getExistServerHistory(serverId);
-    console.log(
-      `found ${history.length} history records for server with id ${serverId} in the system`
+    console.info(
+      `[history] found ${history.length} history records for server with id ${serverId} in the system`
     );
-    // Respond with success message
     res.status(200).json(history);
   } catch (error) {
-    console.error("❌ Error getExistServerHistory server:", error);
+    console.error("[history] Error getExistServerHistory server:", error);
     res
       .status(error.status ? error.status : 500)
       .json({ error: error.message || "Internal Server Error" });
@@ -31,17 +30,23 @@ const handleIsServerHealthyByTimestamp = async (req, res) => {
 
     // If timestamp is invalid, throw an error
     if (isNaN(validTimestamp.getTime())) {
-      throw new Error("Invalid timestamp format.");
+      throw { status: 400, message: "Invalid timestamp format." };
     }
-    // Call the getExistServerHistory function with the data
-    const isHealthy = await getIsServerHealthy(serverId, validTimestamp);
 
-    // Respond with success message
+    const isHealthy = await getIsServerHealthy(serverId, validTimestamp);
+    console.info(
+      `[history] found server  ${
+        isHealthy ? "was" : "was not"
+      } active on time ${timestamp}`
+    );
     res.status(200).json(isHealthy);
   } catch (error) {
-    console.error("❌ Error handleIsServerHealthyByTimestamp history:", error);
+    console.error(
+      "[history] Error handleIsServerHealthyByTimestamp history:",
+      error
+    );
     res
-      .status(error.status ? error.status : 500)
+      .status(error.status || 500)
       .json({ error: error.message || "Internal Server Error" });
   }
 };
